edbc2cb61041a160cdac9500567296851a19cf8b
saba parveen <psaba2040@gmail>
2024-09-20 19:36:44 +0530

    first commit

---
+++ src/Context/StoreContext.jsx
@@ -0,0 +1,53 @@
+import { createContext, useState,useEffect } from "react";
+import { food_list } from "../assets/assets";
+
+export const StoreContext=createContext(null)
+const StoreContextProvider=(props)=>{
+
+    const[cartItems,setCartItem]=useState({});
+    const addToCard=(itemId)=>{
+        if(!cartItems[itemId]){
+            console.log(cartItems[itemId])
+            setCartItem((prev)=>({...prev,[itemId]:1}))
+        }
+        else{
+            setCartItem((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+
+        }
+    }
+     
+    const removeFromCard=(itemId)=>{
+        setCartItem((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+
+    }
+    const getTotalCartAmount=()=>{
+        let totalAmount=0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo=food_list.find((product)=>product._id===item);
+                totalAmount+=itemInfo.price*cartItems[item];
+            }    
+        } 
+        return totalAmount;
+    }
+    //Used to check the cartItem in console.log
+    /* useEffect(() => {
+      console.log(cartItems);
+    }, [cartItems]); */
+
+
+    const contextValue={
+        food_list,
+        cartItems,
+        setCartItem,
+        addToCard,
+        removeFromCard,
+        getTotalCartAmount
+    }
+    return(
+        <StoreContext.Provider value={contextValue}>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+export default StoreContextProvider;
